2a084f2b38da622ed0d8890d6ddbea8f04a8b272
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-03-16 21:39:22 +0300

    user can registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,18 @@
 class ApplicationController < ActionController::Base
   include Pundit
   include Pagy::Backend
+
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :phone, :email, :password) }
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:first_name, :last_name, :phone, :email, :password, :current_password)
+    end
+  end
 end
