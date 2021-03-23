4119fac6a8a4047b6999e78eb41743a78769cd17
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-03-23 20:05:05 +0300

    rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,32 +7,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :trackable, :omniauthable,
          omniauth_providers: [:facebook]
 
-
   validates :email, presence: true, length: { maximum: 63 }
   validates :email, uniqueness: { case_sensitive: false }
 
   validates :password, format: { with: /\A(?=.*\d)(?=.*[A-Z])(?=.*[a-z])[^ ]{8,}\z/ }
 
-
   def self.from_omniauth(auth)
-    name_split = auth.info.name.split(" ")
-    user = User.where(email: auth.info.email).first
-    user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email, password: Devise.friendly_token[0, 20])
+    user = User.where.first(email: auth.info.email)
+    user ||= User.create!(provider: auth.provider, uid: auth.uid, email: auth.info.email,
+                          password: Devise.friendly_token[0, 20])
     user
   end
-
-  # def self.new_with_session(params, session)
-  #   super.tap do |user|
-  #     if data = session[“devise.facebook_data”] && session[“devise.facebook_data”][“extra”][“raw_info”]
-  #       user.email = data[“email”] if user.email.blank?
-  #     end
-  #   end
-  # end
-
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token[0,20]
-  #   end
-  # end
 end
