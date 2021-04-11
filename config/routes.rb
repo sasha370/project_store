d5a6bee10fd923ba9fd56590c09fa0fb1cd9fc1a
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-04-11 14:17:43 +0300

    add Privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: 'pages#home'
+  get 'privacy_policy', to: 'pages#privacy_policy'
+  get 'privacy_policy', to: 'pages#privacy_policy'
+
 
   resources :books, only: %i[index show]
 
