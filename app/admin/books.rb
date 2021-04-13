88557a4ba7fa5408b0cd05980d46178e3137d2e1
Aleksandr Karyakin <kng.sasha@mail.ru>
2021-04-13 21:00:11 +0300

    forth cleanup

--- app/admin/books.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# ActiveAdmin.register Book do
-#   permit_params do
-#     permitted = %i[title author price quantity description published_year dimentions materials]
-#     permitted << :other if params[:action] == 'create' && current_user.admin?
-#     permitted
-#   end
-# end
