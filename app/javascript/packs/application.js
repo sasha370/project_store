33bf82a53aed2dadee13df4ba8d836cca00fe85f
Aleksandr Karyakin <kng.sasha@mail.ru>
2022-01-22 09:33:09 +0300

    sd

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("jquery");
 require("@rails/ujs").start()
 require("turbolinks").start();
 require("@rails/activestorage").start();
@@ -15,7 +14,5 @@ require("stylesheets/application.scss")
 
 
 //= require active_admin/base
-// Support component names relative to this directory:
-var ReactRailsUJS = require("react_ujs");
-var componentRequireContext = require.context("components", true);
-ReactRailsUJS.useContext(componentRequireContext);
+
+require("./server_rendering")
