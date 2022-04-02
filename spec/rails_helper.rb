9f8f961d74a6e2e38cd00605522cbf1fcd24cdc6
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2022-04-02 17:07:43 +0300

    Test 3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,30 +16,6 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-require 'selenium/webdriver'
-require 'webdrivers/chromedriver'
-
-Webdrivers::Chromedriver.required_version = '74.0.3729.6'
-
-Capybara.server = :puma, { Silent: true }
-
-Capybara.register_driver :chrome do |app|
-  Capybara::Selenium::Driver.new(app, browser: :chrome)
-end
-
-Capybara.register_driver :headless_chrome do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: {
-      args: %w[no-sandbox headless disable-gpu window-size=1280,800]
-    }
-  )
-
-  Capybara::Selenium::Driver.new app,
-                                 browser: :chrome,
-                                 desired_capabilities: capabilities
-end
-
-Capybara.javascript_driver = :headless_chrome
 
 RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
