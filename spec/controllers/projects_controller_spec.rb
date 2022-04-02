1b6ab4235cbfdbb2b0d03ee0a2b488607bae33b6
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2022-04-02 17:31:10 +0300

    Fix

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe ProjectsController, type: :controller do
     context 'with projects' do
       let(:category1) { create(:category) }
       let(:category2) { create(:category) }
-      let(:projects1) { create_list(:project, 3, category: category1) }
+      let!(:projects1) { create_list(:project, 2, category: category1) }
       let(:projects2) { create_list(:project, 3, category: category2) }
 
       it 'assigns the requested projects to ALL @projects' do
@@ -27,7 +27,7 @@ RSpec.describe ProjectsController, type: :controller do
 
       it 'assigns the requested projects to Category`s @projects' do
         get :index, params: { category_id: category1.id }
-        expect(assigns(:projects)).to eq(projects1.map(&:decorate))
+        expect(assigns(:projects)).to eq(projects1)
       end
     end
 
