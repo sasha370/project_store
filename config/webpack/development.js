230445de7b48d2957f270fa365dcd3222b8e708e
Aleksandr Kariakin <aleksandr.kariakin@toptal.com>
2022-05-28 16:04:08 +0300

    Webpack cleaner

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -1,5 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const environment = require('./environment')
 
+const webpackConfig = {
+  plugins: [
+    new CleanWebpackPlugin(),
+  ],
+};
 module.exports = environment.toWebpackConfig()
